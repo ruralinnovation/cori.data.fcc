a2862d6785652a4319b0f2f2489f04fbd10f695e
Olivier Leroy <leroy.oli@proton.me>
2024-05-07 16:23:43 -0400

    add fcc provider list has a supported data set

---
+++ R/fcc_provider.R
@@ -0,0 +1,15 @@
+#' FCC providers list
+#'
+#' FCC list of preoviders downloaded in May 2024
+#'
+#' @format ## `fcc_provider`
+#' A data frame with 4,341 rows and 5 columns:
+#' \describe{
+#'   \item{Provider.Name}{Provider name}
+#'   \item{Affiliation}{Affiliation}
+#'   \item{Operation.Type}{ILEC or not}
+#'   \item{FRN}{FCC Registration Number, number of the entity that submited the data}
+#'   \item{Provider.ID}{Provider ID} 
+#' }
+#' @source <https://us-fcc.app.box.com/v/bdcprovideridtable>
+"fcc_provider"
