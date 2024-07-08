1b471695bdac769a972679752d2363065646d439
Olivier Leroy <leroy.oli@proton.me>
2024-07-08 10:11:45 -0400

    small cleaning and adding a very first version of the dl function

---
+++ R/dl_nbm.R
@@ -0,0 +1,57 @@
+#' Download NBM data
+#'
+#' Just a draft of a function that download all NBM data related to CORI works
+#' It motsly works in my setup (download in ~/data_swamp)
+#' 
+#' @param path_to_dl a string by default "~/data_swamp"
+#' @param release_date a string can be "December 31, 2023" or "June 30, 2023" 
+#' @param data_type a string "Fixed Broadband"
+#' @param data_category a string "Nationwide"
+#' 
+#' @return A lot of zipped file 
+#' @export
+#'
+#' @examples
+#' system("mkdir -p  ~/data_swamp")
+#' nbm_data <- dl_nbm(release_date = "June 30, 2023")
+
+
+dl_nbm <- function(path_to_dl = "~/data_swamp", release_date = "June 30, 2023",
+                   data_type = "Fixed Broadband",
+                   data_category = "Nationwide") {
+  # clean my mess
+  prev_timeout <- getOption("timeout")
+  on.exit(options(timeout = prev_timeout))
+  options(timeout = max(360, getOption("timeout")))
+
+
+  base_url <- "https://broadbandmap.fcc.gov/nbm/map/api/getNBMDataDownloadFile/"
+  user_agent <- paste0("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+                       " AppleWebKit/537.36 (KHTML, like Gecko)",
+                       " Chrome/112.0.0.0 Safari/537.36")
+
+  all_data_to_dl <- get_nbm_available()
+  one_release_to_dl <- all_data_to_dl[all_data_to_dl$release == release_date, ]
+  one_release_to_dl <-
+    one_release_to_dl[one_release_to_dl$data_type == data_type, ] 
+  one_release_to_dl <-
+    one_release_to_dl[one_release_to_dl$data_category == data_category, ]
+
+  print(str(one_release_to_dl))
+
+  for (i in seq_len(nrow(one_release_to_dl))) {
+
+    dest_file <- paste0(path_to_dl, "/",
+                        one_release_to_dl$file_name[i],
+                        ".zip")
+
+    if (file.exists(dest_file)) {
+      print(paste(dest_file, "already downloaded, skipping it"))
+      next
+    }
+
+    try(download.file(url = paste0(base_url, one_release_to_dl$id[i], "/1"),
+                      destfile =  dest_file,
+                      headers = c("User-Agent" = user_agent)))
+  }
+}
