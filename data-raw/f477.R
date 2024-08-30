d46e8dbe88d313429829e99572208ebd381aa6fc
Olivier Leroy <leroy.oli@proton.me>
2024-08-30 11:50:52 -0400

    list of url

--- data-raw/f477.R
+++ data-raw/f477.R
@@ -3,7 +3,6 @@
 # the process to get f477 is a bit long
 
 list_url <- c(
-  "https://www.fcc.gov/state-level-form-477-broadband-deployment-data-december-2014-version-1",
   "https://www.fcc.gov/form-477-broadband-deployment-data-december-2014-version-3",
   "https://www.fcc.gov/form-477-broadband-deployment-data-june-2015-version-5",
   "https://www.fcc.gov/form-477-broadband-deployment-data-december-2015-version-4",
@@ -15,8 +14,8 @@ list_url <- c(
   "https://www.fcc.gov/form-477-broadband-deployment-data-december-2018-version-3",
   "https://www.fcc.gov/form-477-broadband-deployment-data-june-2019-version-2",
   "https://www.fcc.gov/form-477-broadband-deployment-data-december-2019-version-1",
-
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2020-version-2",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2020",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2021",
   "https://us-fcc.box.com/v/US-without-Sat-Dec2021-v1"
-
-
 )
