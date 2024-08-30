b505fc6ad72a92fe96b9858cb0bfc9ecd39f8cde
Olivier Leroy <leroy.oli@proton.me>
2024-08-30 08:24:36 -0400

    slow progress on doc

--- data-raw/f477.R
+++ data-raw/f477.R
@@ -1,3 +1,22 @@
 ## code to prepare `f477` dataset goes here
 
 # the process to get f477 is a bit long
+
+list_url <- c(
+  "https://www.fcc.gov/state-level-form-477-broadband-deployment-data-december-2014-version-1",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2014-version-3",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2015-version-5",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2015-version-4",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2016-version-4",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2016-version-2",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2017-version-3",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2017-version-3",
+  "https://www.fcc.gov/fixed-broadband-deployment-data-june-2018-version-1",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2018-version-3",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-june-2019-version-2",
+  "https://www.fcc.gov/form-477-broadband-deployment-data-december-2019-version-1",
+
+  "https://us-fcc.box.com/v/US-without-Sat-Dec2021-v1"
+
+
+)
