7338b1b84387b5e3cdc6ace1e3f281f3651d2455
Olivier Leroy <leroy.oli@proton.me>
2024-09-10 14:31:32 -0400

    improve readme with f477

--- R/f477.R
+++ R/f477.R
@@ -2,7 +2,7 @@
 #'
 #' Source data: FCC Form 477
 #'
-#' @param us_states a directory containing only the csv needed
+#' @param state_abbr a string matching state abbr
 #'
 #' @return a data frame
 #'
@@ -12,10 +12,10 @@
 #'
 #' @examples
 #' \dontrun{
-#' NC <- get_f477(us_states = "NC")
+#' NC <- get_f477(state_abbr = "NC")
 #' }
 
-get_f477 <- function(us_states) {
+get_f477 <- function(state_abbr) {
 
   con <- DBI::dbConnect(duckdb())
   DBI::dbExecute(con,
@@ -24,23 +24,11 @@ get_f477 <- function(us_states) {
 
   DBI::dbExecute(con, "INSTALL httpfs;LOAD httpfs")
 
-  # would it be needed on public ?
-  keyID <- Sys.getenv("AWS_ACCESS_KEY_ID")
-  region <- Sys.getenv("AWS_DEFAULT_REGION")
-  acces_key <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
-  statement_keyID <- sprintf("SET s3_access_key_id='%s'", keyID)
-  statement_region <- sprintf("SET s3_region='%s'", region)
-  statement_acces_key <- sprintf("SET s3_secret_access_key='%s'", acces_key)
-
-  DBI::dbExecute(con, statement_keyID)
-  DBI::dbExecute(con, statement_region)
-  DBI::dbExecute(con, statement_acces_key)
-
   statement <- sprintf("select *
                        from  
                       read_parquet(
-                      's3://fcc-data-cori/f477/*/*/*.parquet')
-                      where StateAbbr = '%s';", us_states)
+                      's3://cori.data.fcc/f477/*/*/*.parquet')
+                      where StateAbbr = '%s';", state_abbr)
 
   DBI::dbGetQuery(con, statement)
 
