ee0193477b752e25e2d8ec7ab8e8c0ecea4d0d23
Olivier Leroy <leroy.oli@proton.me>
2024-09-13 08:42:47 -0400

    unzip

--- data-raw/NBM.R
+++ data-raw/NBM.R
@@ -10,7 +10,7 @@ nbm_data <- get_nbm_available()
 
 system(sprintf("mkdir -p %s", dir))
 
-# this is a big loop 
+# this is a big loop
 for (i in release$filing_subtype){
   dl_nbm(
     path_to_dl = "data_swamp/nbm",
@@ -19,12 +19,18 @@ for (i in release$filing_subtype){
     data_category = "Nationwide")
 }
 
-num_files <- nbm_data |> dplyr::filter(release == "June 30, 2023" &
-                                       data_type == "Fixed Broadband" &
-                                       data_category == "Nationwide") |>
+num_files <- nbm_data |>
+  dplyr::filter(data_type == "Fixed Broadband" &
+                  data_category == "Nationwide") |>
   nrow()
-
+# echking if we have all the files
 files_dl <- length(list.files(dir, pattern = "*.zip"))
 
-identical(num_files, files_dl)
+stopifnot("we are missing some files" = identical(num_files, files_dl))
 
+system(sprintf("mkdir -p %sraw", dir))
+
+system(sprintf("unzip %s\\*.zip -d %sraw", dir, dir))
+
+system(sprintf("du -sh %sraw", dir))
+# 290G    data_swamp/nbm/raw
