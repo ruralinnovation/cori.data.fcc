547aa47eef729d11f9d8e90078e29d1dc7f2f63d
Olivier Leroy <leroy.oli@proton.me>
2024-12-09 15:33:40 -0500

    add referer

--- R/get_nbm_available.R
+++ R/get_nbm_available.R
@@ -37,6 +37,7 @@ get_nbm_available <- function(
                "-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8' ",
                "-H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br, zstd' ",
                "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' ",
+               "-H 'Referer: https://broadbandmap.fcc.gov/data-download'",
                "-H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-User: ?1' ",
                "-H 'Sec-GPC: 1' -H 'Priority: u=0, i' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers'"
         ),
