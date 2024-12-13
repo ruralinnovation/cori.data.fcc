91fdfa6d92a22fbdc574c9ab6f734795b9f43281
Olivier Leroy <leroy.oli@proton.me>
2024-12-13 11:05:59 -0500

    fix on my computer ...

--- R/get_nbm_available.R
+++ R/get_nbm_available.R
@@ -5,7 +5,7 @@
 #' paste0("https://broadbandmap.fcc.gov/nbm/",
 #'        "map/api/national_map_process/nbm_get_data_download/")
 #' ```
-#' @param get_data_url a string providing NBM filing API.
+#' @param get_root_url a string providing NBM filing API.
 #' @param user_agent a string set up by default
 #' 
 #' @return A data frame.
@@ -16,12 +16,13 @@
 #' head(nbm_data)
 
 get_nbm_available <- function(
-  get_data_url = paste0("https://broadbandmap.fcc.gov/nbm/map/",
+  get_root_url = paste0("https://broadbandmap.fcc.gov/nbm/map/",
                         "api/national_map_process/nbm_get_data_download/"),
   user_agent = the$user_agent) {
 
+  # get csv to dl only get a table with all link to be downloaded
   get_csv_to_dl <- function(release_file, release_nb) {
-    get_data_url <- paste0(get_data_url,
+    get_data_url <- paste0(get_root_url,
                            release_file[release_nb, "process_uuid"])
     # h <- curl::new_handle()
     # curl::handle_setheaders(h, "User-Agent" = user_agent)
@@ -37,7 +38,7 @@ get_nbm_available <- function(
                "-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8' ",
                "-H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br, zstd' ",
                "-H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' ",
-               "-H 'Referer: https://broadbandmap.fcc.gov/data-download'",
+               "-H 'Referer: https://broadbandmap.fcc.gov/data-download' ",
                "-H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-User: ?1' ",
                "-H 'Sec-GPC: 1' -H 'Priority: u=0, i' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers'"
         ),
